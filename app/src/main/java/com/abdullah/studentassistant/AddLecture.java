b5a68b82661f89647b5363db5dad45d32c99500f
Abdullahassem <Abdullahassemf@gmail.com>
2023-07-07 21:12:04 +0300

    Added days checkBoxes

--- app/src/main/java/com/abdullah/studentassistant/AddLecture.java
+++ app/src/main/java/com/abdullah/studentassistant/AddLecture.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ public class AddLecture extends AppCompatActivity {
     private EditText etLectureName, etStartTime, etEndTime, etDays;
     private Button btnAddLecture;
     private DatabaseHelper dbHelper;
+    private CheckBox[] checkBoxes = new CheckBox[7];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +27,15 @@ public class AddLecture extends AppCompatActivity {
         etLectureName = findViewById(R.id.etLectureName);
         etStartTime = findViewById(R.id.etStartTime);
         etEndTime = findViewById(R.id.etEndTime);
-        etDays = findViewById(R.id.etDays);
+//        etDays = findViewById(R.id.etDays);
         btnAddLecture = findViewById(R.id.btnAddLecture);
+        checkBoxes[0] = findViewById(R.id.sunday_checkbox);
+        checkBoxes[1] = findViewById(R.id.monday_checkbox);
+        checkBoxes[2] = findViewById(R.id.tuesday_checkbox);
+        checkBoxes[3] = findViewById(R.id.wednesday_checkbox);
+        checkBoxes[4] = findViewById(R.id.thursday_checkbox);
+        checkBoxes[5] = findViewById(R.id.friday_checkbox);
+        checkBoxes[6] = findViewById(R.id.saturday_checkbox);
 
         dbHelper = new DatabaseHelper(this);
 
@@ -36,17 +45,38 @@ public class AddLecture extends AppCompatActivity {
                 String name = etLectureName.getText().toString().trim();
                 String startTime = etStartTime.getText().toString().trim();
                 String endTime = etEndTime.getText().toString().trim();
-                String days = etDays.getText().toString().trim();
+                String days="";
+                String daysChecked = "";
 
-                if (name.isEmpty() || startTime.isEmpty() || endTime.isEmpty() || days.isEmpty()) {
+                for (int i = 0; i < checkBoxes.length; i++) {
+                    if (checkBoxes[i].isChecked()) {
+                        daysChecked += checkBoxes[i].getText().toString();
+                    }
+                }
+                if (name.isEmpty() || startTime.isEmpty() || endTime.isEmpty() || daysChecked.isEmpty() ) {
                     Toast.makeText(AddLecture.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 } else {
-                    Lecture lecture = new Lecture(0, name, startTime, endTime, days);
-                    dbHelper.insertLecture(lecture);
-                    Toast.makeText(AddLecture.this, "Lecture added successfully", Toast.LENGTH_SHORT).show();
+//                    Lecture lecture = new Lecture(0, name, startTime, endTime, days);
+//                    dbHelper.insertLecture(lecture);
+//                    Toast.makeText(AddLecture.this, "Lecture added successfully", Toast.LENGTH_SHORT).show();
+                    addToDb(checkBoxes,name,startTime,endTime);
                     finish(); // Close the activity and return to the previous one
                 }
             }
         });
+    }
+
+    public String addToDb(CheckBox[] checkBoxes,String name,String startTime,String endTime) {
+        String days = "";
+        for (int i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].isChecked()) {
+                days += checkBoxes[i].getText().toString();
+                Lecture lecture = new Lecture(0, name, startTime, endTime, days);
+                dbHelper.insertLecture(lecture);
+                days="";
+            }
+        }
+        Toast.makeText(AddLecture.this, "Lecture added successfully", Toast.LENGTH_SHORT).show();
+        return days;
     }
 }
